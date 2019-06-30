532df10724a22146a02a70664862a30b1fe1854f
sanjeyac <sanjeyac>
2019-06-30 15:41:37 +0200

    launch xdo command to simulate a keyboard shortcut

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::process::Command;
+
+fn key_press(keys: String) {
+    Command::new("xdotool")
+        .arg("key")
+        .arg(keys)
+        .spawn()
+        .expect("failed to execute process"); 
 }
+
+
+fn main() {
+    key_press("alt+Tab".to_string());
+}
+
