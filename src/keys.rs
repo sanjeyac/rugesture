9a2f8c2ff6b03e7aa88bbbd9d30cd7e344f06447
Sanjeya C <sanjeya.cooray@gmail.com>
2019-07-05 23:01:40 +0200

    Dev (#1)
    * launch xdo command to simulate a keyboard shortcut
    
    * reading gesture from libinput debug-events
    
    * minimal refactor
    
    * added toml reading dependencies
    
    * created configuration module
    
    * created gesture module
    
    * key press module
    
    * setup first proptotype
    
    * added some comments

---
+++ src/keys.rs
@@ -0,0 +1,10 @@
+use std::process::Command;
+
+pub fn key_press(keys: &String) {
+    Command::new("xdotool")
+        .arg("key")
+        .arg(keys)
+        .spawn()
+        .expect("failed to execute process"); 
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,71 @@
-fn main() {
-    println!("Hello, world!");
+mod keys;
+mod configuration;
+mod gesture;
+
+use configuration::*;
+use gesture::*;
+use std::process::{Command, Stdio};
+use std::io::{BufRead, BufReader, Error, ErrorKind};
+use std::num::ParseFloatError;
+
+fn execute_command(line: &String, last_update: &mut String, config: &configuration::Config){    
+    
+    match compute(line, last_update) {
+        Some(gesture) => {
+
+            if gesture.0 == 3 {
+                match gesture.1 {
+                    GestureDirection::UP => keys::key_press(&config.three_finger_swipe.up),
+                    GestureDirection::DOWN => keys::key_press(&config.three_finger_swipe.down),
+                    GestureDirection::LEFT => keys::key_press(&config.three_finger_swipe.left),
+                    GestureDirection::RIGHT => keys::key_press(&config.three_finger_swipe.right),
+                    GestureDirection::NONE => ()
+                }
+            }
+            
+            if gesture.0 == 4 {
+                match gesture.1 {
+                    GestureDirection::UP => keys::key_press(&config.four_finger_swipe.up),
+                    GestureDirection::DOWN => keys::key_press(&config.four_finger_swipe.down),
+                    GestureDirection::LEFT => keys::key_press(&config.four_finger_swipe.left),
+                    GestureDirection::RIGHT => keys::key_press(&config.four_finger_swipe.right),
+                    GestureDirection::NONE => ()
+                }
+            }
+        },
+        None => ()
+    }
+
+}
+
+
+fn main() -> Result<(),Box<Error>>{
+
+    // temp variable
+    let mut last_update = "".to_string();
+    let LIBINPUT_DEBUG_COMMAND = "libinput-debug-events";
+
+    // read the configurations from the settings file
+    // each gesture has a key that will be pressed described in the configuration file
+    let config = read_config(&"Settings.toml".to_string()).unwrap();
+
+    
+    // run lib input command to catch gesture events
+    let stdout = Command::new(LIBINPUT_DEBUG_COMMAND)
+        .stdout(Stdio::piped())
+        .spawn()?
+        .stdout
+        .ok_or_else(|| Error::new(ErrorKind::Other,"Could not capture standard output."))?;
+
+    // reader of lines of gesture events
+    let reader = BufReader::new(stdout);
+
+    // for each gesture event execute a command from the config file
+    reader
+        .lines()
+        .filter_map(|line| line.ok())
+        .for_each(|line| execute_command( &line, &mut last_update, &config ));
+
+    Ok(())
+
 }
