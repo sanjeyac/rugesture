6345fea12c265d3642e13a0e51c90322a6861ce7
sanjeyac <sanjeyac>
2019-07-05 21:04:03 +0200

    key press module

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,6 @@
-use std::process::Command;
-
-fn key_press(keys: String) {
-    Command::new("xdotool")
-        .arg("key")
-        .arg(keys)
-        .spawn()
-        .expect("failed to execute process"); 
-}
-
+mod keys;
 
 fn main() {
-    key_press("alt+Tab".to_string());
+    keys::key_press("alt+Tab".to_string());
 }
 
